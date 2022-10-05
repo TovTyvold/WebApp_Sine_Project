9a419fcc884c2621b304ad060334bf9c1d0e9485
Tov Tyvold <tov.tyvold@bouvet.no>
2022-10-05 09:42:41 +0200

    Added updated sine.py and added a call_mod.py for testing it as a module

--- sine.py
+++ sine.py
@@ -1,21 +1,42 @@
+  
 import numpy as np
 import matplotlib.pyplot as plt 
 
 
 list = [1, 3, 5, 7]
+interval = [-2*np.pi, 2*np.pi]
+sample_rate = 200
 A = 1
-def hertz_sine(list, A):
+def hertz_sine(list, A, interval, sample_rate):
+    """  
+        Function that outputs a normalized graph corresponding to several frquencies using sine functions
+        Arguments
+        ---------
+        list - dtype list/touple: A list of the submitted frequencies
+        A - dtype int: The submitted amplitude
+        interval - dtype list/touple: The given interval for our x-cooridnates
+        sample_rate - dtype int: The sample rate
+
+        Returns
+        --------
+        length (int): Number of submitted frequencies
+        x_cord (ndarray): Array containing x-values for intervall
+        y_cord (ndarray): n-dimensional matrix containing all y-values from different hertz
+        norm_max (ndarray): Array containing y-values that are normalized
+
+    """
     length = len(list) 
 
+
     # Create arrays to store points
-    x_cord = np.linspace(-2*np.pi, 2*np.pi, 200)
+    x_cord = np.linspace(interval[0], interval[1], sample_rate)
     y_cord = np.zeros((length,len(x_cord)))
     
     row = 0
     # Run through each Hertz and the y-coordinate they give
     for hz in list:
         for i in range(len(x_cord)):
-            y_cord[row][i] = A * np.sin(hz * x_cord[i])
+            y_cord[row][i] = A * np.sin(2 * np.pi * hz * x_cord[i])
         row = row + 1
 
     # Normalize through maximum
@@ -23,19 +44,11 @@ def hertz_sine(list, A):
     for i in range(length):
         y_sum += y_cord[i][:] 
     norm_max = y_sum/np.max(y_sum)
-
-    # Normalize through signal theory
-    norm_sum = 0
-    for i in range(length):
-        norm_sum += (y_cord[i] * y_cord[i])
-    norm_signal = y_sum/(np.sqrt(norm_sum/(length**2)))
-
-    
-    return length, x_cord, y_cord, norm_max, norm_signal
+    return length, x_cord, y_cord, norm_max, 
 
         
 
-length, x, y, norm_max, norm_signal = hertz_sine(list, A)
+length, x, y, norm_max = hertz_sine(list, A, interval, sample_rate)
 
 
 fig, ax = plt.subplots(1,length,figsize=(10,8),sharex=True)
@@ -48,11 +61,7 @@ plt.tight_layout()
 
 plt.figure(2)
 plt.plot(x,norm_max,'r--')
-
 plt.title("Normalization through maximum")
 
-plt.figure(3)
-plt.plot(x,norm_signal,'b')
 
-plt.title("Normalization through signal theory")
-plt.show()
+plt.show() 
