b4dbbd1572169415662940f382c2d484385edf64
Fabian Gaspero Beckstrøm <fabian.beckstrom@bouvet.no>
2022-10-06 09:26:15 +0200

    changed from onChange to useRef

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,54 @@
-
-import { useForm } from './useForm';
+// App.tsx
 import './App.css';
+import React, { useState, useRef } from 'react';
 
-
-const API_URL = 'https://localhost:5000/test';
-
+// Globals
+const API_URL = 'https://localhost:5000/';
 
 function App() {
 
-  const initialState = {
-    freq1: null,
-    freq2: null,
-    freq3: null
+  // Hooks
+  const freqRef1 = useRef<HTMLInputElement>(null);
+  const freqRef2 = useRef<HTMLInputElement>(null);
+  const freqRef3 = useRef<HTMLInputElement>(null);
+  const sampleRef = useRef<HTMLInputElement>(null);
+
+  const onSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const freq1 = freqRef1?.current?.value;
+    const freq2 = freqRef2?.current?.value;
+    const freq3 = freqRef3?.current?.value;
+    const samples = sampleRef?.current?.value;
+
+    // Create data object to send via API
+    if(freq1 && freq2 && freq3 && samples) {
+      const data = {
+        samples: samples,
+        freqs: [parseInt(freq1), parseInt(freq2), parseInt(freq3)]
+      }
+      sendData(data);
+    } else {
+      console.log('Error: One or more inputs is null or undefined');
+    }
   }
 
-  const { onChange, onSubmit, values } = useForm(sendData, initialState);
 
- 
-  async function sendData(): Promise<any> {
-    console.log(JSON.stringify(values));
+  async function sendData(data: Object): Promise<any> {
+  
+
+    console.log(JSON.stringify(data));
+
     try {
       let response = await fetch(API_URL, {
         method: 'POST',
-        body: JSON.stringify(values)
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
       })
-      return response.json();
+      console.log(response.json());
+      
     } catch (error) {
       console.log(error);
     }
@@ -34,15 +58,33 @@ function App() {
 
   return (
     <div className="App">
+    <>
       <form onSubmit={onSubmit}>
         <div>
-          <input type="number" name='freq1' onChange={onChange} />
-          <input type="number" name='freq2' onChange={onChange} />
-          <input type="number" name='freq3' onChange={onChange} />
+          <label htmlFor='freq1'>
+            1st Frequency
+            <input type="number" name='freq1' id='freq1' ref={freqRef1} required />
+          </label>
+          <label htmlFor="freq2">
+            2nd Frequency
+            <input type="number" name='freq2' id='freq2' ref={freqRef2} required />
+          </label>
+          <label htmlFor="freq3">
+            3rd Frequency
+            <input type="number" name='freq3' id='freq3' ref={freqRef3} required />
+          </label>
+          <label htmlFor="samples">
+            Samples
+            <input type="number" name='samples' id='samples' ref={sampleRef} required/>
+          </label>
           <button type='submit'>Generate</button>
         </div>
       </form>
+     
+      
+    </>
     </div>
+    
   );
 }
 
