445e315e2008cc6ee2d147e69c936924c3175bdd
Brage Bakkane <brage.bakkane@bouvet.no>
2022-10-11 16:51:27 +0200

    custom sine in sound

--- backend/apiServer.py
+++ backend/apiServer.py
@@ -64,34 +64,33 @@ class PointsAnswer(BaseModel):
             }
         }
 
+
 class FreqQuery(BaseModel):
-    funcs: List[Dict[ str, Union[float, int, str] ]]
+    funcs: List[Dict[str, Union[float, int, str]]]
     seconds: Optional[float] = 1.0
     samples: Optional[int] = 400
 
     class Config:
         schema_extra = {
-            "example": { 
-                "funcs" : [{"shape": "sin", "frequency": 2, "amplitude": 1}], 
+            "example": {
+                "funcs": [{"shape": "sin", "frequency": 2, "amplitude": 1}],
                 "seconds": 1.0,
                 "samples": 400
             }
         }
 
+def FreqQueryToPoints(query: Dict, debug=False):
+    funcs = [pointsCalculation.parseDict(func) for func in query["funcs"]]
+    l = pointsCalculation.getPoints(funcs, query["samples"], debug=debug, seconds=query["seconds"])
+
+    return l
+
+
 @app.post("/points", response_model=PointsAnswer)
 async def getPoints(query: FreqQuery):
-    funcs = [pointsCalculation.parseJSON(func) for func in query.funcs]
-    l = pointsCalculation.getPoints(funcs, query.samples, debug=True, seconds=query.seconds)
+    l = FreqQueryToPoints(query.dict(), debug=True)
 
-    #convert l to a dictionary
-    d = []
-    for p in l:
-        x = p[0]
-        y = p[1]
-
-        d.append({"x": x, "y": y})
-
-    return {"points": d}
+    return {"points": [{"x": x, "y": y} for (x, y) in zip(*l)]}
 
 chunkSize = 1024
 samplesCount = 44100
@@ -99,9 +98,12 @@ samplesCount = 44100
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
 
-    await websocket.send_text(str(samplesCount))
+    json = await websocket.receive_json()
+    waveData = FreqQueryToPoints(json, debug=False)[1]
+    await websocket.send_json({"samples": samplesCount, "byteType": "float"})
 
-    cb = soundGen.samplesToCB([y for (_,y) in pointsCalculation.getPoints([ pointsCalculation.parseJSON({"shape" : "sin", "frequency" : 440, "amplitude" : 1}) ], 44100, debug=False)])
+    cb = soundGen.samplesToCB(waveData)
+
     data = cb.readChunk(chunkSize)
     while data:
         await websocket.send_bytes(data)
