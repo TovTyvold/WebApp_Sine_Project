42bd2bccf57ca8a229851508e6ba89cd381cf08d
TovTyvold <70606051+TovTyvold@users.noreply.github.com>
2022-10-12 08:38:37 +0200

    Update pointsFrequency.py

--- backend/pointsFrequency.py
+++ backend/pointsFrequency.py
@@ -31,7 +31,44 @@ def signal_to_hertz(normalized_signal, Fs):
             freq_list.append(f[k])
 
     
+    print("Checking frequencies...")
+    sorted_rand_freq = np.sort(list_rand_freq)
+    f, Pxx, freq_list, amp_list = chaos_to_hertz(norm_max)
+    #np. set_printoptions(threshold=np. inf)
+    tol = 1
+    for k in range(n_signals):
+        if tol < abs(sorted_rand_freq[0][k] - freq_list[k]):
+            print(sorted_rand_freq[0][k])
+            print(freq_list[k])
+            print("Not Alike")
+            print("-------------")
+        else:
+            print(sorted_rand_freq[0][k])
+            print(freq_list[k])
+            print("-------------")
 
+
+    print("mean freq:", np.mean(freq_list))
+    print("-------------")
+    freq_mean2 = np.mean(freq_list)
+    for j in range(len(freq_list)-1):
+        if abs(freq_list[j+1] - freq_list[j]) > freq_mean2:
+            freq_list = freq_list[:j]
+            break 
+
+
+
+
+    print("-----------------------------")
+    np.set_printoptions(threshold=sys.maxsize)
+    print(f"Number of elements in freq_list: {np.shape(freq_list)[0]:.3f}")
+    print(freq_list)
+    print(f"Number of elements in list_rand_freq: {np.shape(list_rand_freq)[1]:.3f}")
+    print(np.sort(list_rand_freq))
+
+    """ print(f"All the frequencies are, in ascending order, {freq_list}")
+    print(f"All the amplitudes are, in ascending order, {amp_list}")
+     """
 
     return freq_list
 
