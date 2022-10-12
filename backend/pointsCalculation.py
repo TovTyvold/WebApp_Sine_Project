341d69e09a1aeb93fa7ada3b6ce87bc1b69c69fe
Brage Bakkane <brage.bakkane@bouvet.no>
2022-10-12 14:26:13 +0200

    adsr functionality and sending of datapoints

--- backend/pointsCalculation.py
+++ backend/pointsCalculation.py
@@ -2,18 +2,19 @@ import math
 from enum import Enum
 from typing import Union, List, Tuple
 import random
+import ADSR_mod
 import functools
 
-def square(amplitude: float, frequency: int, x: float) -> float:
+def square(amplitude: float,  frequency: float, x: float) -> float:
     return 2 * amplitude * (2 * math.floor(frequency * x) - math.floor(2 * frequency * x) + 1 - 0.5)
 
-def saw(amplitude: float, frequency: int, x: float) -> float:
+def saw(amplitude: float,  frequency: float, x: float) -> float:
     return 2 * amplitude * frequency * (x % (1 / frequency)) - 1
 
-def sin(amplitude: float, frequency: int, x: float) -> float:
+def sin(amplitude: float,  frequency: float, x: float) -> float:
     return amplitude * math.sin(2 * math.pi * frequency * x)
 
-def triangle(amplitude: float, frequency: int, x: float) -> float:
+def triangle(amplitude: float,  frequency: float, x: float) -> float:
     return amplitude * (4 * abs(frequency * x - math.floor(frequency * x + 0.75) + 0.25) - 1)
 
 def whitenoise(amplitude: float) -> float:
@@ -35,7 +36,7 @@ conversionTable = {
 class PeriodicFunc:
     shape: str
     amplitude: float
-    frequency: int
+    frequency: float
 
     def __init__(self, shape, amplitude, frequency):
         self.shape = shape
@@ -110,37 +111,48 @@ def parseSine(data : dict) -> List[float]:
             l = ([parseSine(f) for f in data[k]] )
             return list(functools.reduce(lambda xs, ys: map(lambda x, y: x+y, xs, ys), l))
 
-l = parseSine({"+":
-               [
-                   {
-                        "envelope" : {
-                            "points": {
-                                "+": [
-                                    {"wave": {"shape": "sin", "frequency": 1, "amplitude": 1}},
-                                    {"wave": {"shape": "sin", "frequency": 1, "amplitude": 1}}
-                                ],
-                            },
-                            "A": {
-                                "num": 1
-                            },
-                            "D": {
-                                "num": 1
-                            },
-                            "S": {
-                                "num": 1
-                            },
-                            "R" : {
-                                "num": 1
-                            }
-                        }
-                   },
-                   {
-                       "wave": {
-                           "shape": "sin", "frequency": 3, "amplitude": 2
-                       }
-                   }
-               ]
-               })
 
-for i in range(10):
-    print("(" + str(i/10) + ", " + str(l[i]) +")")
+if __name__ == "__main__":
+    l = parseSine({"+":
+                   [
+                       {
+                           "envelope": {
+                               "points": {
+                                   "+": [
+                                       {"wave": {"shape": "sin",
+                                                 "frequency": 1, "amplitude": 1}},
+                                       {"wave": {"shape": "sin",
+                                                 "frequency": 1, "amplitude": 1}}
+                                   ],
+                               },
+                               "A": {
+                                   "num": 1
+                               },
+                               "D": {
+                                   "num": 1
+                               },
+                               "S": {
+                                   "num": 1
+                               },
+                               "R": {
+                                   "num": 1
+                               }
+                           }
+                       },
+                       {
+                           "wave": {
+                               "shape": "sin", "frequency": 3, "amplitude": 2
+                           }
+                       }
+                   ]
+                   })
+
+    for i in range(10):
+        print("(" + str(i/10) + ", " + str(l[i]) + ")")
+
+    #def getPoints(funcs : List[PeriodicFunc], sampleRate: int, debug: bool = False, seconds : float = 1.0) -> Tuple[List[float], List[float]]:
+    points = getPoints([parseDict({"shape": "sin", "frequency": 2, "amplitude": 1})], 100, debug = False, seconds = 4)
+    points = (points[0], ADSR_mod.ADSR(points[1], [1,1,1,1], 100*4, sustainAmplitude=0.7, maxAmplitude=1))
+    
+    for p in zip(*points):
+        print(str(p[0]) + ", " + str(p[1]))
