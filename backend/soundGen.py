f8207dfcdc3311a45c17fc5a084c57d36cc12b30
Brage Bakkane <brage.bakkane@bouvet.no>
2022-10-19 12:45:45 +0200

    added support for two channel audio

--- backend/soundGen.py
+++ backend/soundGen.py
@@ -1,42 +1,60 @@
+from re import L
 import numpy as np
 import struct
 import pyaudio
 import pointsCalculation
-from typing import List
+import functools
+from typing import List, Tuple, Union
 
 samplesCount = 44100
+
 
 class ChunkedBytes:
     bytesRead = 0
     b = bytes()
 
-    def __init__(self, b : bytes):
+    def __init__(self, b: bytes):
         self.b = b
 
     def readChunk(self, size):
-        out = self.b[self.bytesRead : self.bytesRead + size]
+        out = self.b[self.bytesRead: self.bytesRead + size]
         self.bytesRead += size
         return out
 
     def empty(self):
         return len(self.b) <= self.bytesRead
 
-def play(samples):
-    samples = samplesToCB(samples)
+
+def play(samples, dim=1):
+    cachedS = samplesToCB(samples)
 
     p = pyaudio.PyAudio()
     stream = p.open(format=pyaudio.paFloat32,
-            channels=1,
-            rate=samplesCount,
-            output=True)
+                    channels=dim,
+                    rate=samplesCount,
+                    output=True)
 
-    while not samples.empty():
-        stream.write(samples.readChunk(1024))
+    while not cachedS.empty():
+        stream.write(cachedS.readChunk(1024))
 
     stream.stop_stream()
     stream.close()
 
     p.terminate()
 
-def samplesToCB(samples : List[float]) -> ChunkedBytes:
-    return ChunkedBytes(struct.pack("%sf" % len(samples), *samples))
+#if samples is multidimensional then interleave the datapoints
+#i.e. ([l0,l1,l2], [r0,r1,r2]) -> [l0,r0,l1,r1,l2,r2, ...]
+def samplesToCB(samples: Union[List[float], Tuple]) -> ChunkedBytes:
+    if type(samples) is tuple:
+        samples = list(zip(*samples))
+        samples = [lr for pair in samples for lr in pair]
+
+    bytesTuple = ChunkedBytes(struct.pack("%sf" % len(samples), *samples))
+    return bytesTuple
+
+
+if __name__ == "__main__":
+    #test multi-channel input
+    play((pointsCalculation.parse(pointsCalculation.noteNameToSamples("C6"), samples=44100, seconds=5),
+          pointsCalculation.parse(pointsCalculation.noteNameToSamples("C#4"), samples=44100, seconds=5)),
+         dim=2)
