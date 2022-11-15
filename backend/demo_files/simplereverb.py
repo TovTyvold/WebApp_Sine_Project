9822be31808c9c42357be2ce58911405a0b1071f
TovTyvold <tov.tyvold0gmail.com>
2022-11-15 07:33:18 +0100

    Update for demo

--- backend/demo_files/simplereverb.py
+++ backend/demo_files/simplereverb.py
@@ -20,7 +20,7 @@ def Create_Sine(amplitudes, frequencies):
     Fs = 44100
     T = 1/(Fs) 
     N = Fs
-    t = 3
+    t = 2
     t_vec = np.arange(t*N) * (T * t)
     omega = 2* np.pi 
     #sine_add = 0.4*np.sin(np.linspace(-8 * np.pi,0 * np.pi,int(len(t_vec))))
@@ -30,14 +30,12 @@ def Create_Sine(amplitudes, frequencies):
     for i in frequencies:
         y_sum += amplitudes[k] * np.sin(omega * i * t_vec)
     
-    adsr = envelope.getSymmEnv([0.3,0.2,1,0.2], 0.75, 0, t)
+    adsr = envelope.getSymmEnv([0.2,0.2,0.6,0.2], 0.75, 0, t)
     y_sum = list(map(lambda a,b : a*b, bezierCurve.compositeOn(adsr, t_vec), y_sum))
 
     norm_y = y_sum / np.max(np.abs(y_sum))
-    #play(norm_y.copy())
-    combed = dirac_comb_discrete(norm_y.copy(), 5, 10)
-    play(combed)
-    revout = Reverb_(combed.copy(), room_size=1, wet_level = 0.6, dry_level = 0.4, width = 1)
+    play(norm_y.copy())
+    revout = Reverb_(norm_y.copy(), room_size=1, wet_level = 0.8, dry_level = 0.2, width = 1)
     play(revout)
     wav = list(norm_y) + list(np.zeros(int(Fs/2))) + list(revout)
     wave_file(wav, wav2=None, fname="Reverb3.wav", amp=1, sample_rate=44100)
